929f697abe3370009145b1929e27b0883349d327
Steven Inácio <inacio.steven@sentinelsw.de>
2020-08-12 13:30:13 +0200

    Adds string representations for ErrorCodes.

--- include/DataModels/ErrorCode.h
+++ include/DataModels/ErrorCode.h
@@ -4,15 +4,25 @@
 #ifndef ERROR_CODE_H_
 #define ERROR_CODE_H_
 
-    typedef enum ErrorCodes_public {
-        E_OK = 0x00,
-        E_NotResponding,
-        E_NegativeResponse,
-        E_Pending,
-        E_Busy,
-        E_NotSupported,
-        E_ComSpeedNotAdjusted,
-        ErrorCode_amount
-    } UDS_Client_Error_t;
+#define FOREACH_CODE(CODE) \
+        CODE(E_OK) \
+        CODE(E_NotResponding) \
+        CODE(E_NegativeResponse) \
+        CODE(E_Pending) \
+        CODE(E_Busy) \
+        CODE(E_NotSupported) \
+        CODE(E_ComSpeedNotAdjusted) \
+        CODE(ErrorCode_Amount)
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+
+typedef enum ErrorCode_public {
+    FOREACH_CODE(GENERATE_ENUM)
+} UDS_Client_Error_t;
+
+static const char *ErrorCode_STRING[] = {
+    FOREACH_CODE(GENERATE_STRING)
+};
 
 #endif // ERROR_CODE_H_
--- src/Ports/Windows/src/main.c
+++ src/Ports/Windows/src/main.c
@@ -6,6 +6,9 @@
 #include <windows.h>
 #include <stdarg.h>
 
+#define str(x) #x
+#define xstr(x) str(x)
+
 extern void pipe_init(void);
 
 extern TimerInterface timer1;
@@ -16,7 +19,7 @@ uint8_t rxBuffer[RX_BUFFER_LENGTH];
 
 void printMessage(UDS_Client_Error_t error, uint8_t* receive, uint32_t receive_length) {
     if(error != E_OK) {
-        UDS_LOG_WARNING("Got Error response with: 0x%02x", error);
+        UDS_LOG_WARNING("Got Error response %s (0x%02x)", ErrorCode_STRING[error], error);
     }
     if(receive != NULL) {
         printf("[UDS Client] CALLBACK: 0x");
@@ -43,35 +46,48 @@ void uds_wait() {
 
 
 void Setup() {
-    UDS_DCM_ECUReset(UDS_SoftReset);
+    //UDS_DCM_ECUReset(UDS_SoftReset, printMessage);
 }
 
 void DCM() {
     Setup();
+    UDS_LOG_INFO("Set Access Timing Parameter to 0xFFFF and 0xFFFF");
     UDS_DCM_AccessTimingParameter(0xFFFF, 0xFFFF, printMessage);
     uds_wait();
+    UDS_LOG_INFO("Clearing ResponseOnEvents");
     UDS_DCM_ClearResponseOnEvents(printMessage);
     uds_wait();
+    UDS_LOG_INFO("Setting %s (0x%02x) on %s (0x%02x) with subnet 0x00 and Node IDs of 0x0000", xstr(ComCtrl_enableRxAndTx), ComCtrl_enableRxAndTx, xstr(Com_normalCommunicationMessages), Com_normalCommunicationMessages);
     UDS_DCM_CommunicationControl(ComCtrl_enableRxAndTx, Com_normalCommunicationMessages, 0x00, 0x0000, printMessage);
     uds_wait();
+    UDS_LOG_INFO("Enabling DTCs 0x01, 0x02, 0x03 and 0x04");
     UDS_DCM_ControlDTCSetting(0x01, (uint8_t[]){0x01, 0x02, 0x03, 0x04}, 4, printMessage);
     uds_wait();
+    UDS_LOG_INFO("Setting Session to %s (0x%02x)", xstr(UDS_Session_Default), UDS_Session_Default);
     UDS_DCM_DiagnosticSessionControl(UDS_Session_Default, printMessage);
     uds_wait();
+    UDS_LOG_INFO("Resetting with Resettype %s (0x%02x)", xstr(UDS_SoftReset), UDS_SoftReset);
     UDS_DCM_ECUReset(UDS_SoftReset, printMessage);
     uds_wait();
+    UDS_LOG_INFO("Get All Active Response Events");
     UDS_DCM_GetActiveResponseEvents(printMessage);
     uds_wait();
+    UDS_LOG_INFO("Verify Link Control with Fixed Parameter %s (0x%02x)", xstr(PC9600Baud), PC9600Baud);
     UDS_DCM_LinkControl_verifyWithFixedParameter(PC9600Baud, printMessage);
     uds_wait();
-    UDS_DCM_LinkControl_verifyWithSpecificParameter(0x2580, printMessage);
+    UDS_LOG_INFO("Verify Link Control with Specific %d Baud. 0x%04x", 9600, 9600);
+    UDS_DCM_LinkControl_verifyWithSpecificParameter(9600, printMessage);
     uds_wait();
+    UDS_LOG_INFO("Trying to get a Response on Change of DID.");
     UDS_DCM_ResponseOnChangeOfDataIdentifier(false, 0x04, 0x0001, SID_DiagnosticSessionControl, NULL, 0, printMessage, printMessage);
     uds_wait();
+    UDS_LOG_INFO("Trying to get a Response when DID 0x0001 is LessThan 0xFFFF with a 10\% Hysterisis");
     UDS_DCM_ResponseOnComparisonOfValues(false, 0x03, 0x0001, LessThan, 0xFFFF, 0x0A, false, 0x02, 0x0001, SID_DiagnosticSessionControl, 0x0001, printMessage, printMessage);
     uds_wait();
+    UDS_LOG_INFO("Trying to get a Response on DTC Status Change on DTC Mask 0xFF.");
     UDS_DCM_ResponseOnDTCStatusChange(false, 0x03, 0xFF, SID_DiagnosticSessionControl, NULL, 0, printMessage, printMessage);
     uds_wait();
+    UDS_LOG_INFO("Trying to get a Security Seed on Funtion 0x03");
     UDS_DCM_SecurityAccess(0x03, (uint8_t[]){0x00, 0x00, 0x00, 0x00}, 4, printMessage);
     uds_wait();
 }
@@ -102,13 +118,7 @@ int main(void) {
     pipe_init();
     UDS_LOG_INFO("Init Client.");
     UDS_Client_Init(&wincom1, &timer1, NULL, rxBuffer, RX_BUFFER_LENGTH);
-    UDS_LOG_INFO("Trying to change Session.");
-    UDS_DCM_DiagnosticSessionControl(UDS_Session_Default, printMessage);
-    uds_wait();
-    UDS_DCM_ECUReset(UDS_SoftReset, printMessage);
-    uds_wait();
-    UDS_DCM_AccessTimingParameter(P2_DEFAULT, P2_STAR_DEFAULT, printMessage);
-    uds_wait();
+    DCM();
     UDS_LOG_INFO("Exitting...");
     exit(0);
 }
