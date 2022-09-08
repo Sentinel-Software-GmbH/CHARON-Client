08bb5fe746e436d41779e53bd4e6b42c76876fbb
Timo Kristall <Kristall.timo@sentinelsw.de>
2022-09-08 11:56:57 +0200

    WIP integrate doxygen fixing missing file problems.

--- documentation.bat
+++ documentation.bat
@@ -3,7 +3,20 @@ WHERE doxygen
 IF %ERRORLEVEL% NEQ 0 (
 	ECHO doxygen wasn't found
 ) else (
+	IF exist "build\" (
 	doxygen "documentation/Doxyfile" 2> documentation_error.log
 	cd build\documentation\html
 	copy Charon_UDS_Client.chm ..\
+	cd..
+	cd..
+	cd..
+	) else ( 
+	mkdir build
+	doxygen "documentation/Doxyfile" 2> documentation_error.log
+	cd build\documentation\html
+	copy Charon_UDS_Client.chm ..\
+	cd.. 
+	cd.. 
+	cd..
+	)
 )
